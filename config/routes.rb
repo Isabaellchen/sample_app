f6226864ff30b11c721b8172be7594508b6a0525
Isabelle Sandow <isabelle.sandow@web.de>
2011-10-11 23:45:24 +0200

    Finished first cut of the User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 SampleApp::Application.routes.draw do
-  get "users/new"
-
   get "pages/home"
 
   get "pages/contact"
@@ -56,6 +54,7 @@ SampleApp::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  resources :users
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
